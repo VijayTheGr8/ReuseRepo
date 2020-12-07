68adbefbb865b7b31b6427254229045f9044c4ae
ArsalKhan1 <karsalk7@gmail.com>
2020-12-07 12:50:54 -0500

    Tag class and associated test

---
+++ Code/Classes/tag.cs
@@ -0,0 +1,45 @@
+namespace reuseRepo
+{
+    /// <summary>
+    /// A Tag object that can be assigned to an article
+    /// </summary>
+    /// <remarks>
+    /// Part of /Diagram/Article.drawio
+    /// </remarks>
+    public class Tag
+    {
+
+        /// <summary>
+        /// Tag or Tag Phrase
+        /// </summary>
+        private string tag = "";
+
+        /// <summary>
+        /// The class constructor
+        /// </summary>
+        /// <param name="tag"> Initial Tag or Tag Phrase</param>
+        public Tag(string tag)
+        {
+            this.tag = tag;
+        }
+
+        /// <summary>
+        /// Returns the Tag
+        /// </summary>
+        /// <returns> The tag as a string </returns>
+        public string getTag()
+        {
+            return this.tag;
+        }
+
+        /// <summary>
+        /// Returns true if tag is multi-word phrase
+        /// </summary>
+        /// <returns> The isPhrase is a bool that returns true if tag is multi-word phrase. Otherwise, it returns false. </returns>
+        public bool isPhrase()
+        {
+            return this.tag.Split(" ").Length > 1;
+        }
+
+    }
+}
--- Code/Program.cs
+++ Code/Program.cs
@@ -6,7 +6,25 @@ namespace reuseRepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            /// Step 1a: Create 2 tag for a text Article and two tags for a video article
+            reuseRepo.Tag tagTire = new reuseRepo.Tag("tire");
+            reuseRepo.Tag tagChain = new reuseRepo.Tag("Bike Chain");
+            reuseRepo.Tag tagCD = new reuseRepo.Tag("CD");
+            reuseRepo.Tag tagCoaster = new reuseRepo.Tag("Coaster");
+
+            /// Step 1b: Printing the tags
+            Console.WriteLine(tagTire.getTag());
+            Console.WriteLine(tagTire.isPhrase());
+            Console.WriteLine(tagChain.getTag());
+            Console.WriteLine(tagChain.isPhrase());
+            Console.WriteLine(tagCD.getTag());
+            Console.WriteLine(tagCD.isPhrase());
+            Console.WriteLine(tagCoaster.getTag());
+            Console.WriteLine(tagCoaster.isPhrase());
+
+            
         }
     }
 }
