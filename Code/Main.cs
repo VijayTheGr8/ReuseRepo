466c05e632344cf1049c8365e8e5416435a70d4a
ArsalKhan1 <karsalk7@gmail.com>
2020-12-13 15:10:02 -0500

    polishing fixes

--- Code/Main.cs
+++ Code/Main.cs
@@ -28,7 +28,11 @@ namespace reuseRepo
 
 
             // 2. Increases the price for Articles by 10%
+            double textPrice= textArticle.getPrice();
+            Console.WriteLine("The price for text article, " + textArticle.getTitle() + ", is $" + textPrice.ToString("00.00"));
             textArticle.setPrice(textArticle.getPrice() * 1.1);
+            double videoPrice = videoArticle.getPrice();
+            Console.WriteLine("The price for the video article, " + videoArticle.getTitle() + ", is $" + videoPrice.ToString("00.00"));
             videoArticle.setPrice(videoArticle.getPrice() * 1.1);
 
             //3. Prints and Plays text and video articles respectively.
@@ -36,9 +40,9 @@ namespace reuseRepo
             videoArticle.play();
 
             /// 4. (over)Writes Text articles into textArticle.json and Video articals into VideoArticles.json
-            fileContent = textArticle.getJSONString();
+            //fileContent = textArticle.getJSONString();
             //Console.WriteLine(fileContent);
-            File.WriteAllText("C:\\Arsal\\ICS4U\\ReuseRepo\\Code\\Text Files\\textArticle.json", fileContent);
+            //File.WriteAllText("C:\\Arsal\\ICS4U\\ReuseRepo\\Code\\Text Files\\textArticle.json", fileContent);
             fileContent = videoArticle.getJSONString();
             //Console.WriteLine(fileContent);
             File.WriteAllText("C:\\Arsal\\ICS4U\\ReuseRepo\\Code\\Text Files\\VideoArticle.json", fileContent);
